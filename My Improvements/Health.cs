d0089b2ee7d894c289c01f21de246c02b955aa03
joshtempest <joshuabussink29@gmail.com>
2023-07-05 14:26:11 +0200

    My Improvements

---
+++ My Improvements/Health.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _health, _maxHealth = 1;
+    GameObject childToHide;
+    [SerializeField] Image healthbar;
+    public bool shieldActive = false;
+
+    private void Awake()
+    {
+        ResetHealth();
+        childToHide = GameObject.Find("Player/shieldOnPlayer");
+    }
+
+    /// <summary>
+    ///  Make the player take one damage. This function can be overloaded with a int for the damage value.
+    /// </summary>
+    /// <param name="TakeDamage">Makes the player take one damage.</param>
+    /// <returns></returns>
+    public void TakeDamage()
+    {
+        if (shieldActive)
+        {
+            childToHide.SetActive(false);
+            shieldActive = false;
+            return;
+        }
+        _health--;
+        UpdateUI();
+        SoundManager.instance.PlaySound(Sounds.TakeDamage);
+        CheckIfDead();
+    }
+
+    /// <summary>
+    ///  Make the player take a given value of damage.
+    /// </summary>
+    /// <param name="TakeDamage">Makes the player take a given amount of damage.</param>
+    /// <returns></returns>
+    public void TakeDamage(int damage)
+    {
+        if (shieldActive)
+        {
+            childToHide.SetActive(false);
+            shieldActive = false;
+            return;
+        }
+        _health -= damage;
+        UpdateUI();
+        SoundManager.instance.PlaySound(Sounds.TakeDamage);
+        CheckIfDead();
+    }
+
+    /// <summary>
+    ///  Make the player restore one health. This function can be overloaded with a int for the heal value.
+    /// </summary>
+    /// <param name="RestoreHealth">Makes the player restore one health.</param>
+    /// <returns></returns>
+    public void RestoreHealth()
+    {
+        if (_health < _maxHealth)
+        {
+            _health++;
+        }
+        UpdateUI();
+        SoundManager.instance.PlaySound(Sounds.Heal);
+    }
+
+    /// <summary>
+    ///  Make the player restore a given value of health.
+    /// </summary>
+    /// <param name="RestoreHealth">Makes the player restore a given amount of health.</param>
+    /// <returns></returns>
+    public void RestoreHealth(int healing)
+    {
+        if (_health + healing > _maxHealth)
+        {
+            _health = _maxHealth;
+        } else _health += healing;
+        UpdateUI();
+        SoundManager.instance.PlaySound(Sounds.Heal);
+    }
+
+    /// <summary>
+    ///  Resets the player health and set it to max health, this can be used when reseting game.
+    /// </summary>
+    /// <param name="ResetHealth">Resets the player health and set it to max health.</param>
+    /// <returns></returns>
+    public void ResetHealth()
+    {
+        _health = _maxHealth;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        healthbar.fillAmount = (float)_health / _maxHealth;
+    }
+
+    private void CheckIfDead()
+    {
+        if (_health <= 0)
+        {
+            GameManager.instance.GameEnded();
+            _health = 0;
+        }
+    }
+}
